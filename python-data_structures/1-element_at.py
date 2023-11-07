0e4168f7db9a2eae5acc69357b4722d99fcaa76d
nasimwe <n.asimwe@alustudent.com>
2023-11-07 02:27:24 -0800

    Task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx >= 0 and idx < len(my_list):
-        return(my_list[idx])
-    else:
-        return(None)
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    return my_list[idx]
+
