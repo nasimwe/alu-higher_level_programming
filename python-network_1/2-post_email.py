a40d97077b77981c73fa57f17ebb262b81ab8e16
nasimwe <n.asimwe@alustudent.com>
2023-11-25 22:07:45 -0800

    Task 10

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL and an email,
-sends a POST request to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8)"""
+"""
+Module 2-post_email.py
+"""
+
+
+import urllib.parse
 import urllib.request
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read().decode('utf-8')
+        print(page)
